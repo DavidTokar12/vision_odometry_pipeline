fd563ad1dd887779ea247daf90e98ec09233529b
AntoZek <a.zektronix@gmail.com>
2025-12-06 20:44:01 +0100

    Add preprocessing and undistrotion map function

--- src/vision_odometry_pipeline/steps/preprocessing.py
+++ src/vision_odometry_pipeline/steps/preprocessing.py
@@ -15,12 +15,13 @@ class ImagePreprocessingStep(VoStep):
         self, state: VoState, debug: bool
     ) -> tuple[np.ndarray, np.ndarray | None]:
         """
-        Converts the current image to grayscale.
+        Converts the current image to grayscale and removes distortion.
 
         Returns:
-            Normal: (gray_image,)
-            Debug:  (gray_image, visualization_image)
+            Normal: (gray_undistorted_image,)
+            Debug:  (gray_undistorted_image, visualization_image)
         """
+        EqualizeHist = False  # Sometimes helps KLT tracking
 
         # Input is the raw current image
         # Note: We assume the Runner ensures 'curr' is not None before calling steps
@@ -30,11 +31,24 @@ class ImagePreprocessingStep(VoStep):
         gray: np.ndarray
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) if img.ndim == 3 else img
 
+        # Logic: Remove distortion
+        map_x, map_y, roi = state.
+        #this maps need to be computed only once, at the beginning of the pipeline.
+        # see the additional function in folder "steps" called "create_undistorted_maps.py"
+        # that creates this maps.
+        # Run the function in main at the beginning and store the maps and roi in something that
+        #  can be quickly accessed by steps.
+
+        gray_undistorted = cv2.remap(gray, map_x, map_y, interpolation=cv2.INTER_LINEAR)
+        x, y, w, h = roi
+        gray_undistorted = gray_undistorted[y:y+h, x:x+w]
+
         # Optional: Equalize Hist (often helps KLT)
-        gray = cv2.equalizeHist(gray)
+        if EqualizeHist:
+            gray_undistorted = cv2.equalizeHist(gray_undistorted)
 
         if debug:
             # Visualization: Just the grayscale image
-            return gray, gray
+            return gray_undistorted, gray_undistorted
 
-        return gray, None
+        return gray_undistorted, None
