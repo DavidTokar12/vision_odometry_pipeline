0400b8c952a8f457c33a61719fb77c92e4d0f4fa
Tim Wyss <tiwyss@ethz.ch>
2025-12-30 15:56:48 +0000

    try different image processing methods

--- src/vision_odometry_pipeline/steps/preprocessing.py
+++ src/vision_odometry_pipeline/steps/preprocessing.py
@@ -35,6 +35,25 @@ class ImagePreprocessingStep(VoStep):
             # Optional: Mild blur to reduce noise amplified by CLAHE
             gray_undistorted = cv2.GaussianBlur(gray_undistorted, (3, 3), 0)
 
+        if False:
+            gamma = 1.5  # Values < 1.0 brighten the image
+            look_up_table = np.array(
+                [((i / 255.0) ** (1.0 / gamma)) * 255 for i in np.arange(0, 256)]
+            ).astype("uint8")
+            gray_undistorted = cv2.LUT(gray_undistorted, look_up_table)
+
+        if False:
+            # Convert to float for precision
+            img_float = gray_undistorted.astype(np.float32)
+
+            # Apply Log Transform: s = c * log(1 + r)
+            # We calculate 'c' such that the maximum pixel value maps to 255
+            c = 255 / np.log(1 + np.max(img_float))
+            log_image = c * (np.log(img_float + 1))
+
+            # Convert back to uint8
+            gray_undistorted = np.array(log_image, dtype=np.uint8)
+
         # Optional: Equalize Hist
         if False:
             gray_undistorted = cv2.equalizeHist(gray_undistorted)
